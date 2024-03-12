8c3f4050175f0a26903a0d4bbbb480104c5c7018
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-03-12 18:09:08 +0400

    commit

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -6,7 +6,7 @@ type PropType = {
 
 export default function Card({ id, company, position }: PropType) {
   return (
-    <div className="w-[90%] h-40 bg-gray-500 text-center">
+    <div className="w-[85%] h-40 bg-gray-500 text-center">
       <p>{company}</p>
       <p>{position}</p>
       <p>{id}</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,13 @@ import data from './data.json';
 export default async function Home() {
   return (
     <div className="flex items-center justify-center flex-col w-full gap-10">
-      {data.map((el) => (
-        <Card company={`${el.company}`} id={`${el.id}`} position={`${el.position}`} />
+      {data.map((el, key) => (
+        <Card
+          key={key}
+          company={`${el.company}`}
+          id={`${el.id}`}
+          position={`${el.position}`}
+        />
       ))}
     </div>
   );
