605711ae6f544e0a9e50c46c9e86c0cd5e196555
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-03-12 18:02:58 +0400

    commit

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,15 @@
+type PropType = {
+  id: string;
+  company: string;
+  position: string;
+};
+
+export default function Card({ id, company, position }: PropType) {
+  return (
+    <div className="w-[90%] h-40 bg-gray-500 text-center">
+      <p>{company}</p>
+      <p>{position}</p>
+      <p>{id}</p>
+    </div>
+  );
+}
